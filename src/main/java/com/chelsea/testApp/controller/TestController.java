adc1ffcda91bbef8cd4cb9019774cf1f5d7c68b7
Chelsea Zhu <qianwei5668@gmail.com>
2020-01-20 19:01:24 -0800

    add home path;

--- src/main/java/com/chelsea/testApp/controller/TestController.java
+++ src/main/java/com/chelsea/testApp/controller/TestController.java
@@ -1,4 +1,12 @@
 package com.chelsea.testApp.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+    @RequestMapping("/")
+    public String home() {
+        return "Spring boot is running!";
+    }
 }
